e01868af7aece93d6d7d2c5603c45e03ac56cac9
Hingyi  Khong and Krystal Foster <hingyi@>
2018-08-14 13:17:21 -0700

    factored responses using point sytem instead of array comparison

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,30 @@
 $(document).ready(function() {
   $("form#fortune_survey").submit(function(event) {
     event.preventDefault();
-    var badResponses = [];
-    var goodResponses = [];
+    var badResponses = 0;
+    var goodResponses = 0;
 
     $("input:checkbox[name=bad-luck]:checked").each(function(){
-      var badLuckVal = $(this).val();
-      badResponses.push(badLuckVal);
+      // var badLuckVal = $(this).val();
+      // badResponses.push(badLuckVal);
+      badResponses ++;
     });
 
     $("input:checkbox[name=good-luck]:checked").each(function(){
-      var goodLuckVal = $(this).val();
-      goodResponses.push(goodLuckVal);
+      // var goodLuckVal = $(this).val();
+      goodResponses ++;
     });
 
     if (badResponses > goodResponses) {
         $("#bad-result").show();
+
     } else if (goodResponses > badResponses) {
         $("#good-result").show();
+
+    }  else if (goodResponses === badResponses) {
+        $("#neutral-result").show();
     }
-    
+
     $('#fortune_survey').hide();
   });
 });
