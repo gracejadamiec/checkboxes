15cd6289b901f8d2b699a33af718222c20e6b249
Hingyi  Khong and Krystal Foster <hingyi@>
2018-08-14 11:48:50 -0700

    set up checkbox inputs and backend branching logic to display different futures

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,25 @@
 $(document).ready(function() {
-  $("form#transportation_survey").submit(function(event) {
+  $("form#fortune_survey").submit(function(event) {
     event.preventDefault();
-    $("#work-responses").show();
-    $("input:checkbox[name=work-transportation]:checked").each(function(){
-      var workTransportationMode = $(this).val();
-      $('#work-responses').append(workTransportationMode + "<br>");
-    });
-    $("#fun-responses").show();
-    $("input:checkbox[name=fun-transportation]:checked").each(function(){
-      var funTransportationMode = $(this).val();
-      $('#fun-responses').append(funTransportationMode + "<br>");
+    var badResponses = [];
+    var goodResponses = [];
+
+    $("input:checkbox[name=bad-luck]:checked").each(function(){
+      var badLuckVal = $(this).val();
+      badResponses.push(badLuckVal);
     });
 
-  //   $("#fun-responses").show();
-  //   $("input:checkbox[name=work-transportation]:checked").each(function(){
-  //    var workTransportationMode = $(this).val();
-  //    var capitalWorkTransportationMode = workTransportationMode.toUpperCase();
-  //    $('#fun-responses').append(capitalWorkTransportationMode + "<br>");
-  //  });
-  // });
-  //   $('transportation_survey').hide();
-  //
-  // });
-    $('transportation_survey').hide();
+    $("input:checkbox[name=good-luck]:checked").each(function(){
+      var goodLuckVal = $(this).val();
+      goodResponses.push(goodLuckVal);
+    });
+
+    if (badResponses > goodResponses) {
+        $("#bad-result").show();
+    } else if (goodResponses > badResponses) {
+        $("#good-result").show();
+    }
+    
+    $('#fortune_survey').hide();
   });
 });
